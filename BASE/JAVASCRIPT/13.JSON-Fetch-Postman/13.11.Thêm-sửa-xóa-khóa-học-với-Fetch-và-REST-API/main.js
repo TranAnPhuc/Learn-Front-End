6f39f4666d5cf0d5c0512d19ca0675303f37af13
TranAnPhuc <anphuctran24@gmail.com>
2025-05-25 15:15:03 +0700

    .

--- BASE/JAVASCRIPT/13.JSON-Fetch-Postman/13.11.Thêm-sửa-xóa-khóa-học-với-Fetch-và-REST-API/main.js
+++ BASE/JAVASCRIPT/13.JSON-Fetch-Postman/13.11.Thêm-sửa-xóa-khóa-học-với-Fetch-và-REST-API/main.js
@@ -24,22 +24,84 @@ function getCourses(callback) {
 function createCourse(data, callback) {
   var options = {
     method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
     body: JSON.stringify(data),
   };
   fetch(courseApi, options)
     .then(function (response) {
-      return response.json();
+      response.json();
     })
     .then(callback);
+}
+
+function handleDeleteCourse(id) {
+  var options = {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  };
+  fetch(courseApi + "/" + id, options)
+    .then(function (response) {
+      response.json();
+    })
+    .then(function () {
+      var courseItem = document.querySelector(".course-item-" + id);
+      if (courseItem) {
+        courseItem.remove();
+      }
+    });
+}
+
+function handleUpdateCourse(id) {
+  var courseItem = document.querySelector(".course-item-" + id);
+  var courseName = courseItem.querySelector("h4").innerText;
+  var courseDescription = courseItem.querySelector("p").innerText;
+  document.querySelector("input[name='name']").value = courseName;
+  document.querySelector("input[name='description']").value = courseDescription;
+
+  var createBtn = document.querySelector("#create");
+  createBtn.innerHTML = "Sửa";
+  createBtn.onclick = function () {
+    var newValueName = document.querySelector("input[name='name']").value;
+    var newValueDescription = document.querySelector(
+      "input[name='description']"
+    ).value;
+    var newFormData = {
+      name: newValueName,
+      description: newValueDescription,
+    };
+    updateCourses(newFormData, function () {
+      getCourses(renderCourses);
+    });
+    function updateCourses(updateData, callback) {
+      var options = {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(updateData),
+      };
+      fetch(courseApi + "/" + id, options)
+        .then(function (response) {
+          return response.json();
+        })
+        .then(callback);
+    }
+  };
 }
 
 function renderCourses(courses) {
   var listCoursesBlock = document.querySelector("#list-courses");
   var htmls = courses.map(function (course) {
     return `
-    <li>
+    <li class="course-item-${course.id}">
         <h4>${course.name}</h4>
         <p>${course.description}</p>
+        <button onclick="handleDeleteCourse(${course.id})">Xóa</button>
+        <button onclick="handleUpdateCourse(${course.id})">Sửa</button>
     </li>
     `;
   });
@@ -58,6 +120,8 @@ function handleCreateForm() {
       description: description,
     };
 
-    createCourse(formData);
+    createCourse(formData, function () {
+      getCourses(renderCourses);
+    });
   };
 }
